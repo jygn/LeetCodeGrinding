9cbc19901775a6add74107fc86b0e253740fa625
jygn <jessy-g92@hotmail.com>
2022-06-01 21:14:01 -0400

    removeDuplicate

--- EasyProblems.java
+++ EasyProblems.java
@@ -452,16 +452,54 @@ public class EasyProblems {
         traversal(result, node.right);
     }
 
+    /** 
+     * 
+     * TODO https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+        Given an integer array nums sorted in non-decreasing order, 
+        remove the duplicates in-place such that each unique element appears only once. 
+        The relative order of the elements should be kept the same.
+
+        Since it is impossible to change the length of the array in some languages, 
+        you must instead have the result be placed in the first part of the array nums. 
+        More formally, if there are k elements after removing the duplicates, 
+        then the first k elements of nums should hold the final result.
+        It does not matter what you leave beyond the first k elements.
+
+        Return k after placing the final result in the first k slots of nums.
+
+        Do not allocate extra space for another array. 
+        You must do this by modifying the input array in-place with O(1) extra memory.
+     */
+    public static int removeDuplicates(int[] nums) {
+        
+        int k = 0;
+
+        for (int i = nums.length-1; i > 0; i--) {
+
+            int current = nums[i];
+            int left = nums[i-1];
+
+            if (left == current) {
+                nums[i] = nums[i+1];
+                nums[i+1] = current;
+                k++;
+            }
+        }
+
+        return k;
+    }
+
     public static void main(String args[]) {
 
         long start = System.nanoTime();
+        
+        int[] t = new int[]{0,0,1,1,1,2,2,3,3,4};
 
-        TreeNode treeNode = new TreeNode(1);
-        treeNode.left = null;
-        treeNode.right = new TreeNode(2);
-        treeNode.right.left = new TreeNode(3);
-
-        System.out.println(inorderTraversal(treeNode));
+        System.out.println(removeDuplicates(t));
+        
+        for (int i : t) {
+            System.out.print(i + ",");
+        }
 
         long end = System.nanoTime();
 
