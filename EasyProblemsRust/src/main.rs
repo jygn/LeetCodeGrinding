195b8f2c31b27e4b45963fbc1d88306f22ff202c
jygn <jessy-g92@hotmail.com>
2022-07-18 16:22:04 -0400

    r

--- EasyProblemsRust/src/main.rs
+++ EasyProblemsRust/src/main.rs
@@ -1,4 +1,4 @@
-use std::cmp::max;
+use std::{cmp::max, collections::{HashSet}};
 
 // Given two strings s and goal, return true if you can swap two letters in s so the result is equal to goal, otherwise, return false.
 // Swapping letters is defined as taking two indices i and j (0-indexed) such that i != j and swapping the characters at s[i] and s[j].
@@ -73,23 +73,74 @@ pub fn maximum_product(nums: Vec<i32>) -> i32 {
     max(max3*max2*max1.unwrap(), 
     max1.unwrap()*min2*min1.unwrap())
 }
-
   
+// There is a malfunctioning keyboard where some letter keys do not work. All other keys on the keyboard work properly.
+// Given a string text of words separated by a single space (no leading or trailing spaces) 
+// and a string brokenLetters of all distinct letter keys that are broken, 
+// return the number of words in text you can fully type using this keyboard.
+pub fn can_be_typed_words(text: String, broken_letters: String) -> i32 {
+    let mut brkn_set = HashSet::new();
+    for c in broken_letters.chars() {
+        brkn_set.insert(c);
+    }
+    let mut res = 0;
+    let mut brkn_w = false;
+    for c in text.chars() {
+        if c == ' ' {
+            if !brkn_w {
+                res += 1
+            }
+            brkn_w = false;
+        }
+        match brkn_set.get(&c) {
+            Some(val) => {
+                if c == *val {
+                    brkn_w = true
+                }
+            }, None => {}
+        }
+    }
+    if !brkn_w {
+        res+=1
+    } 
+    res
+}
+
+// A shop is selling candies at a discount. For every two candies sold, the shop gives a third candy for free.
+// The customer can choose any candy to take away for free as long as the cost of the chosen candy is 
+// less than or equal to the minimum cost of the two candies bought.
+// For example, if there are 4 candies with costs 1, 2, 3, and 4, and the customer buys candies with costs 2 and 3, 
+// they can take the candy with cost 1 for free, but not the candy with cost 4.
+// Given a 0-indexed integer array cost, where cost[i] denotes the cost of the ith candy, return the minimum cost of buying all the candies.
+pub fn minimum_cost(cost: Vec<i32>) -> i32 {
+    let mut sorted_cost = cost;
+    sorted_cost.sort_by(|a, b| b.cmp(a));
+
+    let mut candies: u8 = 0;
+    let mut min_cost = 0;
+
+    for v in &sorted_cost {
+        if candies != 2 {
+            min_cost += v;
+            candies += 1
+        } else { 
+            candies = 0
+        }
+    }
+    min_cost
+}
+
 fn f_deref() {
     let mut c = 'a';
-
     println!("c: {}", c);
 
     let b = &mut c;
-
     *b = 'r';
 
     println!("b: {}", b);
     println!("c: {}", c);
 }
-
-
   
 fn main() {
-    println!("{}", maximum_product(vec![1,2,3])); 
+    println!("{}", minimum_cost(vec![6,5,7,9,2,2])); 
 }
